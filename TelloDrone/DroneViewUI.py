d10af8625f2b07e56660732de3bd44406247d52d
planetrobotics <44882732+planetrobotics@users.noreply.github.com>
2022-07-26 18:14:19 -0400

    Changes from Vansh

--- TelloDrone/DroneViewUI.py
+++ TelloDrone/DroneViewUI.py
@@ -20,6 +20,15 @@ class DroneViewUI:
 
         self.root = root
 
+        # Add image file
+        bg = ImageTk.PhotoImage(Image.open('Resources/background.png').resize((width, height)))
+
+        # Show image using label
+        label1 = Label(root, image=bg)
+        label1.place(x=0, y=0)
+
+
+
         self.batteryImage = {
             100 : ImageTk.PhotoImage(Image.open('Resources/battery_full.png').resize((40, 24))),
             75 : ImageTk.PhotoImage(Image.open('Resources/battery_75.png').resize((40, 24))),
@@ -27,18 +36,22 @@ class DroneViewUI:
             25: ImageTk.PhotoImage(Image.open('Resources/battery_25.png').resize((40, 24)))
         }
 
+
+
+
         self.wifiImage = {
             "OK" : ImageTk.PhotoImage(Image.open('Resources/wifi.png').resize((32, 32))),
             "KO" : ImageTk.PhotoImage(Image.open('Resources/no_wifi.png').resize((32, 32)))
         }
 
         self.launchImage = {
-            "launch" : PhotoImage(file='Resources/launch.png'),
-            "land" : PhotoImage(file='Resources/land.png')
+            "launch": ImageTk.PhotoImage(Image.open('Resources/launch.png').resize((76, 50))),
+            "land": ImageTk.PhotoImage(Image.open('Resources/land.png').resize((64, 46)))
+
         }
 
         self.driverAssistImage = {
-            "start": ImageTk.PhotoImage(Image.open('Resources/driverassist.png').resize((256, 128))),
+            "start": ImageTk.PhotoImage(Image.open('Resources/driverassist.png').resize((299, 128))),
             "stop": ImageTk.PhotoImage(Image.open('Resources/driverassist.png').resize((256, 128)))
         }
 
@@ -50,64 +63,89 @@ class DroneViewUI:
         self.launched = False
         self.running = True
 
-        upImage = PhotoImage(file='Resources/up.png')
+        upImage = ImageTk.PhotoImage(Image.open('Resources/up.png').resize((72, 65)))
         upButton = Button(root, image=upImage, borderwidth=0)
-        upButton.place(x=950, y=75)
+        upButton.place(x=843, y=108)
         upButton.bind("<ButtonPress>", self.up_clicked)
         upButton.bind("<ButtonRelease>", self.up_release)
 
-        downImage = PhotoImage(file='Resources/down.png')
+        downImage = ImageTk.PhotoImage(Image.open('Resources/down.png').resize((68, 58)))
         downButton = Button(self.root, image=downImage, borderwidth=0)
-        downButton.place(x=950, y=275)
+        downButton.place(x=843, y=248)
         downButton.bind("<ButtonPress>", self.down_clicked)
         downButton.bind("<ButtonRelease>", self.down_release)
 
-        rightImage = PhotoImage(file='Resources/right.png')
+        rightImage = ImageTk.PhotoImage(Image.open('Resources/right.png').resize((64, 58)))
         rightButton = Button(self.root, image=rightImage, borderwidth=0)
-        rightButton.place(x=1050, y=180)
+        rightButton.place(x=930, y=180)
         rightButton.bind("<ButtonPress>", self.right_clicked)
         rightButton.bind("<ButtonRelease>", self.right_release)
 
-        leftImage = PhotoImage(file='Resources/left.png')
+        leftImage = ImageTk.PhotoImage(Image.open('Resources/left.png').resize((62, 58)))
         leftButton = Button(self.root, image=leftImage, borderwidth=0)
-        leftButton.place(x=850, y=180)
+        leftButton.place(x=765, y=180)
         leftButton.bind("<ButtonPress>", self.left_clicked)
         leftButton.bind("<ButtonRelease>", self.left_release)
 
-        forwardImage = PhotoImage(file='Resources/forward.png')
+        forwardImage = ImageTk.PhotoImage(Image.open('Resources/forward.png').resize((60, 70)))
         forwardButton = Button(self.root, image=forwardImage, borderwidth=0)
-        forwardButton.place(x=745, y=105)
+        forwardButton.place(x=665, y=105)
         forwardButton.bind("<ButtonPress>", self.forward_clicked)
         forwardButton.bind("<ButtonRelease>", self.forward_release)
 
-        reverseImage = PhotoImage(file='Resources/backward.png')
+        reverseImage = ImageTk.PhotoImage(Image.open('Resources/backward.png').resize((60, 80)))
         reverseButton = Button(self.root, image=reverseImage, borderwidth=0)
-        reverseButton.place(x=745, y=250)
+        reverseButton.place(x=665, y=250)
         reverseButton.bind("<ButtonPress>", self.back_clicked)
         reverseButton.bind("<ButtonRelease>", self.back_release)
 
         self.launchText = "launch"
         self.launchBtn = Button(self.root, image=self.launchImage[self.launchText], borderwidth=0, command=self.launch)
-        self.launchBtn.place(x=940, y=185)
+        self.launchBtn.place(x=840, y=185)
 
         self.panel = Label(self.root, image=None, compound="top")
         self.panel.image = self.batteryImage[50]
         self.panel.place(x=0, y=5)
 
         self.assistButton = Button(self.root, image=self.driverAssistImage["start"], borderwidth=0)
-        self.assistButton.place(x=850, y=400)
+        self.assistButton.place(x=718, y=360)
+
+        def openNewWindow():
+            # Toplevel object which will
+            # be treated as a new window
+            newWindow = Toplevel(root)
+
+            # sets the title of the
+            # Toplevel widget
+            newWindow.title("Help")
+
+            # sets the geometry of toplevel
+            newWindow.geometry("500x200")
+
+            # A Label widget to show in toplevel
+            Label(newWindow,
+                  text="" "\nThere are 2 ways to use DroneView:"
+                       "\n\n-Press the manual control buttons to manually move the drone\n \n - Press Driver Assist to make the drone automatically follow the truck\n\n ").pack()
+
+
+
 
         infoImage = Image.open('Resources/info.png')
         infoImage = infoImage.resize((32, 32))
         infoImage = ImageTk.PhotoImage(infoImage)
-        info = Label(self.root, image=infoImage, compound="top")
-        info.place(x=(width - 75), y=5)
+        info = Button(self.root, image=infoImage, compound="top",command=openNewWindow)
+        info.place(x=(width -75), y=5)
+
+
+
+
 
         self.wifi = Label(self.root, image=self.wifiImage["OK"], compound="top")
-        self.wifi.place(x=(width - 110), y=5)
+        self.wifi.place(x=(width - 115), y=5)
 
         self.battery = Label(self.root, image=self.batteryImage[100], compound="top")
-        self.battery.place(x=(width - 150), y=5)
+        self.battery.place(x=(width - 165), y=5)
+        self.batteryLevel = 100;
 
         self.launchThread = None
 
@@ -178,14 +216,14 @@ class DroneViewUI:
             self.launchText = "land"
             self.launchBtn = Button(self.root, image=self.launchImage[self.launchText], borderwidth=0,
                                     command=self.launch)
-            self.launchBtn.place(x=950, y=195)
+            self.launchBtn.place(x=845, y=188)
 
         else:
             self.launchBtn.destroy()
             self.launchText = "launch"
             self.launchBtn = Button(self.root, image=self.launchImage[self.launchText], borderwidth=0,
                                     command=self.launch)
-            self.launchBtn.place(x=940, y=185)
+            self.launchBtn.place(x=840, y=185)
 
     def showCameraFeed(self, image):
         self.panel = Label(self.root, image=image, compound="top")
@@ -200,8 +238,6 @@ class DroneViewUI:
         print("UI closed")
 
     def setBattery(self, batteryLevel):
-        if self.battery != None:
-            self.battery.destroy()
 
         level = 100 #default
         if batteryLevel > 85:
@@ -212,8 +248,13 @@ class DroneViewUI:
             level = 50
         else:
             level = 25
-        self.battery = Label(self.root, image=self.batteryImage[level], compound="top")
-        self.battery.place(x=(self.width - 150), y=5)
+
+        if self.batteryLevel != level:
+            self.batteryLevel = level
+            if self.battery != None:
+                self.battery.destroy()
+            self.battery = Label(self.root, image=self.batteryImage[level], compound="top")
+            self.battery.place(x=(self.width - 165), y=5)
 
     def setWifi(self, wifiLevel):
         if self.wifi != None:
@@ -224,7 +265,7 @@ class DroneViewUI:
             wifiAvailable = "KO"
 
         self.wifi = Label(self.root, image=self.wifiImage[wifiAvailable], compound="top")
-        self.wifi.place(x=(self.width - 110), y=5)
+        self.wifi.place(x=(self.width - 115), y=5)
 
 
 if __name__ == '__main__':
